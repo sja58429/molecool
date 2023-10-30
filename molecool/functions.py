4c273061fa4210126db69f50e7670467fc3d94e0
Steven <steven.ayoub.362@my.csun.edu>
2023-10-30 11:21:29 -0700

    minimum working example for Python coding Style lesson.
    The current repo will allow the user to work on the coding style portion of the lesson plan. Making the functions more readable by changing the variable names within open_pdb and write_xyz functions

--- molecool/functions.py
+++ molecool/functions.py
@@ -1,5 +1,37 @@
+import numpy as np
+
 """Provide the primary functions."""
 
+def open_pdb(f_loc):
+    with open(f_loc) as f:
+        data = f.readlines()
+    c = []
+    sym = []
+    for l in data:
+        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
+            sym.append(l[76:79].strip())
+            c2 = [float(x) for x in l[30:55].split()]
+            c.append(c2)
+    coords = np.array(c)
+    return sym, coords
+
+
+def write_xyz(file_location, symbols, coordinates):
+    
+    ## Write an xyz file given a file location, symbols, and coordinates.
+    num_atoms = len(symbols)
+    
+    if num_atoms != len(coordinates):
+        raise ValueError(f"write_xyz : the number of symbols ({num_atoms}) and number of coordinates ({len(coordinates)}) must be the same to write xyz file!")
+    
+    with open(file_location, 'w+') as f:
+        f.write('{}\n'.format(num_atoms))
+        f.write('XYZ file\n')
+        
+        for i in range(num_atoms):
+            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
+                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
+            
 
 def canvas(with_attribution=True):
     """
